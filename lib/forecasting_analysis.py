2422f3688917d5ec7bc3d0a6bb9efe2997676f80
Nuelam9 <manuellacal@gmail.com>
2022-05-24 14:17:28 +0200

    Splitted analysis.py code in new files

---
+++ lib/forecasting_analysis.py
@@ -0,0 +1,213 @@
+import os
+import numpy as np
+import pandas as pd
+import datetime as dt
+import matplotlib.pyplot as plt
+from preprocessing_analysis import q1, q2
+from utils import latex_settings, fancy_legend
+from statsmodels.tsa.statespace.sarimax import SARIMAX
+from sklearn.metrics import mean_absolute_percentage_error as mape
+
+
+def add_linear_seasonal_terms(df: pd.DataFrame, new_col: str = 'Load_denoised',
+                              signal_col: str = 'Load_real_comp',
+                              col: str = 'Load_lin_seasonal') -> pd.DataFrame:
+    """Add seasonal and trend components to the signal after forecasting. 
+
+    Args:
+        df (pd.DataFrame): data containing the forecasting,
+        new_col (str, optional): new columns' name of the forecasting.
+                                 Defaults to 'Load'.
+        signal_col (str, optional): columns' name of the sum of the 
+                                    seasonal and trend component with 
+                                    the forecasted signal.
+                                    Defaults to 'Load_real_comp'.
+        col (str, optional): columns' name of the seasonal + trend
+                             component.
+                             Defaults to 'Load_lin_seasonal'.
+
+    Returns:
+        pd.DataFrame: data forecasted + the seasonal and trend components.
+    """
+    df = df.add(df[col], axis=0)
+    # remove column used for the summation
+    del df[col]
+    df.columns = df.columns.str.replace(signal_col, new_col)
+    return df
+
+
+def psf_forecasting(df: pd.DataFrame, cut_day: str,
+                    feature: str = 'Load_real_comp',
+                    lin_seasonal: str = 'Load_lin_seasonal') -> pd.DataFrame:
+    """_summary_
+
+    Args:
+        df (pd.DataFrame): _description_
+        cut_day (str): _description_
+        feature (str, optional): _description_. Defaults to 'Load_real_comp'.
+        lin_seasonal (str, optional): _description_. Defaults to 'Load_lin_seasonal'.
+
+    Returns:
+        pd.DataFrame: _description_
+    """
+    train_set = df[df.index < cut_day].copy()
+    test_set = df[df.index >= cut_day].copy()
+    
+    n_days_ahead = len(test_set.index.day_of_year.unique())
+    
+    working_days = train_set[~train_set.Holiday]
+    festivities = train_set[train_set.Holiday]
+    
+    f = {feature: ['mean', q1, q2], lin_seasonal: ['mean', q1, q2]}
+    pred_working_days = working_days.groupby(['Day', 'Hour']).agg(f)
+    pred_festivities = festivities.groupby(['Day id', 'Hour']).agg(f)    
+
+    forecasting = []
+    for day in pd.date_range(cut_day, periods=n_days_ahead, freq="d"):
+        if any(day.day_of_year == festivities['Day id']):
+            forecasting.append(
+                pred_festivities.loc[day.day_of_year, feature].to_numpy()
+                )
+        else:
+            forecasting.append(
+                pred_working_days.loc[day.day_of_week, feature].to_numpy()
+                )
+
+    forecasting = np.concatenate(forecasting)
+    tmp = df[[lin_seasonal, feature]].copy()
+    n = len(forecasting) - len(test_set)
+    #print(n)
+
+    tmp.loc[tmp.index >= cut_day,
+            ['forecast', 'q1_forecast', 'q2_forecast']] = forecasting[n:, :]
+
+    return add_linear_seasonal_terms(tmp)
+
+
+def sarimax_forecasting(df, cut_day, arima_args, seasonal_args):
+    train_set = df[df.index < cut_day]
+    # weekly: (1,0,9) x (2,1,0,52)
+    # daily: (4,0,5) x (1,1,0,7)
+    # hourly: (8,0,30) x (1,1,1,24)
+    model = SARIMAX(
+        train_set, order=arima_args,
+        seasonal_order=seasonal_args,
+        freq=train_set.index.inferred_freq
+        )
+    fit_model = model.fit()
+    forecast = fit_model.get_prediction(
+        start=cut_day,
+        end=df.index[-1]
+        )
+    return forecast.conf_int()
+    
+
+def save_sarimax_results(forecast, original_df, model): 
+    df = forecast.copy()
+    freq = df.index.inferred_freq 
+    
+    if freq == 'W-SUN':
+        n_weeks = (1 + (df.index[-1] - df.index[0]).days) // 7
+        time_interval = n_weeks
+        interval_type = 'weeks'     
+    else:
+        n_days = 1 + (df.index[-1] - df.index[0]).days
+        time_interval = n_days
+        interval_type = 'days'
+    
+    original_df = original_df[original_df.index >= df.index[0]].copy()
+    
+    df['mean Load'] = df.mean(axis=1)
+    df['Load_lin'] = original_df.Load_lin
+    # add the trend to the data
+    df = df.add(df['Load_lin'], axis=0).iloc[:, :-1]
+    df.columns = ['q1_forecast', 'q2_forecast', 'forecast']
+    df.index.name = 'Date'
+
+    file_path = f'../Results/{model}/'
+    file = f"{model}_{freq}_{time_interval}{interval_type}.csv"
+    if os.path.isfile(file_path + file):
+        pass
+    else:    
+        df.to_csv(file_path + file, index=True)
+
+
+def plot_model_forecasting(df, cut_day, model, feature = 'Load',
+                           file=None, ax = None, legend = None, xlabel=None):
+   
+    freq = df.index.inferred_freq 
+    if freq == 'W-SUN':
+        weeks = 52
+        n_weeks = ((df.index[-1] - cut_day).days + 1) // 7
+        time_interval = n_weeks
+        interval_type = 'weeks'     
+    else:
+        weeks = 2
+        n_days = (df.index[-1] - cut_day).days + 1
+        time_interval = n_days
+        interval_type = 'days'
+        
+    date = cut_day - dt.timedelta(weeks=weeks)
+    cut_mask = df.index >= cut_day
+    date_mask = df.index > date
+    
+    if not ax:
+        ax = latex_settings()
+        legend = True
+        xlabel = 'Date'
+        file = f'{model}_{freq}_{time_interval}{interval_type}.png'
+    
+    df.loc[date_mask, [feature]].plot(ax=ax, color='b', lw=1, legend=legend)
+    df.loc[cut_mask, [feature, 'forecast']].plot(ax=ax,
+                                                 color=['g', 'orange'],
+                                                 lw=1, legend=legend)
+    ax.fill_between(x=df[date_mask].index,
+                    y1=df[date_mask].q1_forecast,
+                    y2=df[date_mask].q2_forecast,
+                    color='orange', alpha=0.5, lw=1)
+    ax.grid()
+    ax.set_ylabel('Load (MW)')
+    ax.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
+    ax.set_xlabel(xlabel)
+    ax.set_title(f'Poland Electricity Load ({time_interval} {interval_type})')
+    
+    if legend:
+        leg = plt.legend(loc="lower left", ncol=4, fancybox=True,
+                         framealpha=0.5, fontsize=7.5,
+                         labels=['Train set', 'Test set', 'Forecast', '95% CI'])
+        fancy_legend(leg)
+    else:
+        pass
+    
+    file_path = f'../Images/{model}/'
+    
+    if os.path.isfile(file_path + file):
+        pass
+    else:    
+        plt.savefig(file_path + file, dpi=800, transparent=True)
+
+
+def accuracy_metric(dfs, cut_dates, feature='Load'):
+    accuracy, mapes, rel_err = [], [], []
+    for df in dfs:
+        mask = df['forecast'].notna()
+        tmp = df[mask].copy()
+        
+        rel_err_mean = np.mean(
+            (tmp['q2_forecast'] - tmp['q1_forecast']) / tmp['forecast'])
+        rel_err.append(round(rel_err_mean, 4))
+        
+        x = df.loc[mask, feature]
+        y = df.loc[mask, 'forecast']
+        mapes.append(round(mape(x, y), 4))
+         
+        mask_ci = (tmp['q1_forecast'] <= tmp[feature]) & \
+                  (tmp[feature] <= tmp['q2_forecast'])
+        accuracy.append(round(len(tmp[mask_ci]) / len(tmp), 4))
+        
+    tmp = pd.DataFrame(
+        [cut_dates, accuracy, mapes, rel_err],
+        index=['cut date', 'accuracy', 'mape', 'rel err']
+        ).T
+    tmp.iloc[:, 1:] = tmp.iloc[:, 1:].multiply(100.)
+    return tmp
---
+++ lib/preprocessing_analysis.py
@@ -0,0 +1,180 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import deconvolve
+import pywt
+from statsmodels.tsa.stattools import adfuller, kpss
+import pandas as pd
+
+
+def linear_fit(x, y):
+    m, q = np.polyfit(x=x, y=y, deg=1)
+    load_lin = m * x + q
+    return load_lin
+
+
+def normalize(data, data_max, data_min):
+    return (data - data_min) / (data_max - data_min)
+
+
+def denormalize(data_normalized, data_max, data_min):
+    return data_normalized * (data_max - data_min) + data_min
+
+
+def q1(x):
+    return x.quantile(0.025)
+
+
+def q2(x):
+    return x.quantile(0.975)
+
+
+def fourierExtrapolation(x, n_predict, n_harm=10):
+    n = x.size  
+
+    t = np.arange(0, n)
+    p = np.polyfit(t, x, 1)
+
+    x_notrend = x - p[0] * t - p[1]     # signal detrended 
+    x_freqdom = np.fft.fft(x_notrend)   # signal in frequencies domain
+    f = np.fft.fftfreq(n)               # frequencies
+
+    indexes = list(range(n))
+    indexes.sort(key= lambda i: np.absolute(f[i]))
+
+    t = np.arange(0, n + n_predict)
+    restored_sign = np.zeros(t.size)
+
+    # We exclude the first index because it represent the zero 
+    # frequency, which corresponds to an infinite period
+    for i in indexes[1:1 + n_harm * 2]:
+        amplitude = np.absolute(x_freqdom[i]) / n
+        phase = np.angle(x_freqdom[i])
+        restored_sign += amplitude * np.cos(2 * np.pi * f[i] * t + phase)
+    
+    return restored_sign
+
+
+def wavelet_coeffs_plot(data, waveletname='sym4', figsize=(10, 10),
+                        label_size=10, title_size=14):
+    """Plot of wavelen data and coefficients approximation.
+
+    Args:
+        df ([DataFrame]): [description].
+        waveletname (str): ['coif5', 'sym4', 'sym5'].
+
+    Returns:
+        [tuple[array, array]]: wavelen data and coefficients.
+    """
+    n = len(data)
+    t = np.arange(n)
+    levels = pywt.dwt_max_level(n, waveletname)
+    data_l, c_l = pywt.dwt(data, waveletname)
+    lws = np.array([0.1 if i < 3 else 0.5 for i in range(levels)])
+
+    # Empirical evidence suggests that a good initial guess for the 
+    # decomposition depth is about half of the maximum possible depth
+    dec = 2.
+    fig, axs = plt.subplots(nrows=levels, ncols=2, figsize=figsize,
+                            constrained_layout=True)
+    for i in range(levels):
+        axs[i, 0].plot(data_l, 'r', lw=lws[i])
+        axs[i, 0].set_xlim(t[0] // dec, t[-1] // dec)
+        axs[i, 1].plot(c_l, 'g', lw=lws[i])
+        axs[i, 1].set_xlim(t[0] // dec, t[-1] // dec)
+        axs[i, 0].set_ylabel(f"Level {i + 1}", fontsize=label_size, rotation=90)
+        dec *= 2.
+        if i == 0:
+            axs[i, 0].set_title("Approximation coefficients",
+                                fontsize=title_size, weight='bold')        
+            axs[i, 1].set_title("Detail coefficients", fontsize=title_size,
+                                weight='bold')        
+    plt.show()
+    return data_l, c_l
+
+
+def wavelet_filter(data, wavelet='sym4', threshold=0.04):
+    maxlev = pywt.dwt_max_level(len(data), wavelet)
+
+    # Decompose into wavelet components, to the level selected
+    coeffs = pywt.wavedec(data, wavelet, level=maxlev)
+
+    # Apply threshold to detail coefficients
+    for i in range(1, len(coeffs)):
+        coeffs[i] = pywt.threshold(coeffs[i], mode='soft', 
+                                   value=threshold*max(coeffs[i]))
+    
+    # Multilevel 1D Inverse Discrete Wavelet Transform
+    datarec = pywt.waverec(coeffs, wavelet)
+    return datarec
+
+
+def resampling_data(df, features = ['Load'], resample_cost = 'h'):
+    f = {'Hour': 'mean', 'Minutes': 'mean'} | {feature: 'sum' for feature in features}
+    tmp = df.copy()
+    tmp = tmp.set_index('Date').resample(resample_cost).agg(f)
+    return tmp
+
+
+class StationarityTests:
+    def __init__(self, significance=.05):
+        self.SignificanceLevel = significance
+        self.pValue = None
+        self.isStationary = None
+        
+    def ADF_Stationarity_Test(self, timeseries):
+
+        #Dickey-Fuller test:
+        adfTest = adfuller(timeseries, autolag=None, maxlag=1)
+        
+        self.pValue = adfTest[1]
+        
+        if (self.pValue < self.SignificanceLevel):
+            self.isStationary = 'Yes'
+        else:
+            self.isStationary = 'No'
+        
+        dfResults = pd.Series(
+                              adfTest[0:4],
+                              index=[
+                                  'DF Test Statistic',
+                                  'P-Value',
+                                  '# Lags Used',
+                                  '# Observations Used'
+                                  ]
+                              )
+        
+        # Add Critical Values
+        for key, value in adfTest[4].items():
+            dfResults[f'Critical Value ({key})'] = value
+
+        df = pd.DataFrame(dfResults, columns=['Dickey-Fuller Test Results'])
+        df.loc['Is the time series stationary?', :] = self.isStationary
+        self.Results = df
+    
+    def kpss_Stationarity_Test(self, timeseries):
+        kpssTest = kpss(timeseries, nlags=1)
+        
+        self.pValue = kpssTest[1]
+        
+        if (self.pValue > self.SignificanceLevel):
+            self.isStationary = 'Yes'
+        else:
+            self.isStationary = 'No'
+        
+        dfResults = pd.Series(
+            kpssTest[0:4],
+            index=[
+                'KPSS Test Statistic',
+                'P-Value',
+                '# Lags Used',
+                '# Observations Used'
+                ]
+            )
+        
+        #Add Critical Values
+        for key, value in kpssTest[3].items():
+            dfResults[f'Critical Value ({key})'] = value
+
+        df = pd.DataFrame(dfResults, columns=['KPSS Test Results'])
+        df.loc['Is the time series stationary?', :] = self.isStationary
+        self.Results = df
